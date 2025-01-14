7d400f62b8f9d1165f7cba49ab147474742eed77
AHADI5 <gloireahadi9@gmail.com>
2025-01-14 23:56:57 +0200

    unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,39 +9,67 @@ const App = () => {
     const handleNeutral = () => setNeutral(neutral + 1) ;
     const handleBad   = () => setBad(bad + 1) ;
     const header  = "give feedback" ;
+    const statisticsTitle  = "Satistics" ;
+
+    //More statistics
+
+    const all = good + neutral + bad ; 
+    const average = (good - bad) / all ;
+    const positive = (good / all) * 100 ;
     return (
       <>
         <Header text = {header}/>
         <Button text = "good" handleClick = {handleGood}/>
         <Button text = "neutral" handleClick = {handleNeutral}/>
         <Button text = "bad" handleClick = {handleBad}/>
+        <Header text = {statisticsTitle}/>
+        <Statistics statistics = {{good  , bad  , neutral  , all , average  , positive}} />
 
-        <Display good={good}   neutral={neutral} bad={bad}/>
+        
       </>
     )
 
+}
+
+
+const Statistics  = (props) => {
+  const {good, neutral, bad, all, average, positive} = props.statistics ;
+  if(all === 0){
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else {
+    return  (  
+      <div>
+          <table>
+              <tbody>
+                <StatisticsLine text = "good" value = {good}/>
+                <StatisticsLine text = "neutral" value = {neutral}/>
+                <StatisticsLine text = "bad" value = {bad}/>
+                <StatisticsLine text = "all" value = {all}/>
+                <StatisticsLine text = "average" value = {average}/>
+                <StatisticsLine text = "positive" value = {positive}/>
+              </tbody>
+          </table>
+      </div>
+    )
+  }
 }
 
 export default App;
 
 const Header  = ({text}) => <h1>{text}</h1> ;
 const Button = ({text, handleClick}) => <button onClick = {handleClick}>{text}</button> ;
-const Display = ({good  , neutral , bad}) => {
-  console.log(good)
-  const text = "statistics" ;
-  const all = good + neutral + bad ; 
-  const average = (good - bad) / all ;
-  const positive = (good / all) * 100 ;
+const StatisticsLine =  ({text, value }) => {
   return (
-   <>
-    <h1>{text}</h1>
-    <p>Good {good}</p>
-    <p>Neutral {neutral}</p>
-    <p>Bad {bad}</p>
-    <p>All {all}</p>
-    <p>Average {average}</p>
-    <p>Positive {positive} %</p>
-   </>
+
+      <tr>
+        <td>{text}</td>
+        <td>{value} {text  === "positive" ? "%" : ""}</td>
+      </tr>
 
   )
 }
+
