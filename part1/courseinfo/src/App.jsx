28cc0b3156cca2a97c902ed2c20577f9eb179931
AHADI5 <gloireahadi9@gmail.com>
2025-01-14 16:25:55 +0200

    unicafe step 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,48 +1,58 @@
 const App = () => {
-  const course = 'Half stack application development '
-  const part1 = 'Fundamentals of React'
-  const exercise1  = 10 
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7 
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course  =  {
+    name  :  "Half stack application development " , 
+    parts : [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+  
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+  
+      {
+        name: "State of a component",
+        exercises: 7,
+      },
+    ]
+  }
 
-   return (
+
+  return (
     <div>
-      <Header course = {course}/>
-      <Part part= {part1} exercises = {exercise1}/>
-      <Part part= {part2} exercises = {exercises2}/>
-      <Part part= {part3} exercises = {exercises3}/>
-      <Total total = {exercise1  + exercises2 + exercises3} /> 
+      <Header course={course.name} />
+      <Part part={course.parts} />
+      <Total part={course.parts} />
     </div>
-
-  )
-}
-const Header  = (course) => {
+  );
+};
+const Header = (course) => {
   return (
     <>
       <h1>{course.course}</h1>
     </>
-  )
-}
+  );
+};
 
-const Part  = (Content) => {
-  return  (
-    <>
-        <p>
-            {Content.part} {Content.exercises}
-        </p>
-    </>
-  )
+const Part = (Content) => {
+  const parts = Content.part.map((part) => (
+    <div key={part.name}>
+      <p>
+        {part.name} <span>{part.exercises}</span>
+      </p>
+    </div>
+  ));
+  console.log(parts);
+  return <>{parts}</>;
+};
 
-}
-
-const Total  = (total) => {
-  return  (
-    <>
-      <p>Number of exercises  {total.total}</p>
-    </>
-  )
-}
-export default App
-
+const Total = (Content) => {
+  var total = 0;
+  Content.part.forEach((value) => {
+    total = total + value.exercises;
+  });
+  return <p>Number of exercises {total}</p>;
+};
+export default App;
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const App = () => {
+    const [good  , setGood  ] = useState(0) ; 
+    const [neutral, setNeutral] = useState(0) ;
+    const [bad   , setBad   ] = useState(0)
+
+    const handleGood  = () => setGood(good + 1) ;
+    const handleNeutral = () => setNeutral(neutral + 1) ;
+    const handleBad   = () => setBad(bad + 1) ;
+    const header  = "give feedback" ;
+    return (
+      <>
+        <Header text = {header}/>
+        <Button text = "good" handleClick = {handleGood}/>
+        <Button text = "neutral" handleClick = {handleNeutral}/>
+        <Button text = "bad" handleClick = {handleBad}/>
+
+        <Display good={good}   neutral={neutral} bad={bad}/>
+      </>
+    )
+
+}
+
+export default App;
+
+const Header  = ({text}) => <h1>{text}</h1> ;
+const Button = ({text, handleClick}) => <button onClick = {handleClick}>{text}</button> ;
+const Display = ({good  , neutral , bad}) => {
+  console.log(good)
+  const text = "statistics" ;
+  return (
+   <>
+    <h1>{text}</h1>
+    <p>Good {good}</p>
+    <p>Neutral {neutral}</p>
+    <p>Bad {bad}</p>
+   </>
+
+  )
+}
